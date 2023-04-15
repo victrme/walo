c618325f49302be16e510e50e1741ad996995297
Victor Azevedo <victor.azevedo@mail.com>
2023-04-15 12:06:44 +0200

    Improved login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,19 @@
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, getAuth, signOut } from 'firebase/auth'
 import './Login.css'
 
-export default function Login() {
+export default function Login({ uid }: { uid: string | null }) {
 	const provider = new GoogleAuthProvider()
 	const auth = getAuth()
 
+	function handleLogin() {
+		uid ? signOut(auth) : signInWithPopup(auth, provider)
+	}
+
 	return (
 		<div id='login'>
-			<button onClick={() => signInWithPopup(auth, provider)}>
-				<img src='google.svg' width={20} height={20} />
-				<span>Se connecter avec Google</span>
+			<button onClick={handleLogin}>
+				{uid === null && <img src='google.svg' width={20} height={20} />}
+				<span>{uid ? 'Se déconnecter' : 'Se connecter avec Google'}</span>
 			</button>
 		</div>
 	)
